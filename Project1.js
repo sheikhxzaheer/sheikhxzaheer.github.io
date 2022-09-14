aacabfe4fdc213afe79897a9fe08b7f960cf9c2c
sheikhxzaheer <sheikhzaheer037@gmail.com>
2022-09-14 20:20:45 +0530

    commit

--- Project1.js
+++ Project1.js
@@ -1,3 +1,13 @@
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 50) {
+        nav.classList.add('active_nav');
+    }
+    else {
+        nav.classList.remove('active_nav');
+    }
+})
 function copyToClipboard(element) {
     var $temp = $("<input>");
     $("body").append($temp);
