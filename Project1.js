3c75f9d349243f02b14d1c4c144a1c92d92923d5
sheikhxzaheer <sheikhzaheer037@gmail.com>
2022-09-17 18:13:21 +0530

    commit

--- Project1.js
+++ Project1.js
@@ -3,15 +3,40 @@ window.addEventListener("load", function () {
     loader.style.display = "none";
 })
 
+
 const nav = document.querySelector('nav');
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY >= 300) {
+    if (window.scrollY >= 50) {
         nav.classList.add('active_nav');
     } else {
         nav.classList.remove('active_nav');
     }
 })
+
+var TrandingSlider = new Swiper('.tranding-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2.5,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
+
+
 function copyToClipboard(element) {
     var $temp = $("<input>");
     $("body").append($temp);
